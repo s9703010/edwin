9c0a1a9c64ed2d8a0d0b3d6793f0a0e83595aa7a
DESKTOP-73HSA0I\Edwin <s9703010@gmail.com>
2017-07-06 21:25:36 +0800

    幹

---
+++ index.js
@@ -0,0 +1,21 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId:"1523559534",
+  channelSecret:"3bb342be42492248e1dd6d42af2909de",
+  channelAccessToken: "LjKptyesdfdUxTP/iYJCch+23XUBdIaz5xP8/icp6pCqxpyVsDgxmkrHPNLI1bCjejKCycMq7DvV0DOY7sI5F/+tvwCsSPTlwHRfHJF4HpQ2AWRs0GJ7++Pz4UwlS4BNLo3eWGOxEZw2rF0u5aMtNQdB04t89/1O/w1cDnyilFU="
+});
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
