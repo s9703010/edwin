1eb32ae6c79c57c7d61fd3420cd60f71cf5c5e58
DESKTOP-73HSA0I\Edwin <s9703010@gmail.com>
2017-07-10 13:07:18 +0800

    no message

--- index.js
+++ index.js
@@ -32,4 +32,21 @@ var server = app.listen(process.env.PORT || 8080, function() {
       text: 'hihi',
       content: [Function] },
   reply: [Function] }
+bot.on('message', function(event) {
+  if (event.message.type = 'text') {
+    var msg = event.message.text;
+    event.reply(msg).then(function(data) {
+      // success 
+      console.log(msg);
+    }).catch(function(error) {
+      // error 
+      console.log('error');
+    });setTimeout(function(){
+    var userId = '使用者 ID';
+    var sendMsg = '要發送的文字';
+    bot.push(userId,sendMsg);
+    console.log('send: '+sendMsg);
+},5000);
+  }
+});
 }
