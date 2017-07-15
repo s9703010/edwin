6d14b1426ed4aa2d63a16f253e4b19cf942061b3
DESKTOP-73HSA0I\Edwin <s9703010@gmail.com>
2017-07-15 23:44:42 +0800

    no message

--- index.js
+++ index.js
@@ -92,7 +92,8 @@ bot.on('message',function(event){
 						"type": "postback",
 						"label": "減脂應該如何調整飲食?",
 						"data": "#"
-					// },
+					}
+					// ,
 					// {
 						// "type": "postback",
 						// "label": "增肌應該如何調整飲食?",
