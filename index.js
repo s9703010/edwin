9ff5befb192a9521ad3d1b8d030949c2a84a35e0
DESKTOP-73HSA0I\Edwin <s9703010@gmail.com>
2017-07-15 23:28:44 +0800

    no message

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/', linebotParser);
 
 //express預設是走port 3000，而heroku不是，所以以下程式碼將進行轉換
 
-var server = app.listen(process.env.PORT || 8080,function(){
+var server = app.listen(process.env.PORT || 8000,function(){
     
 var port = server.address().port;
     
